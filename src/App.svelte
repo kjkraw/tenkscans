29600487383de2fe380f7192aa18184c3138955a
Kuy Krawczeniuk <kjkrawczeniuk@gmail.com>
2022-03-12 17:52:29 -0800

    Redesign front page using design version 2

--- src/App.svelte
+++ src/App.svelte
@@ -4,21 +4,22 @@
 
 <main>
   <Navbar/>
-  <section id="hero">
-    <h1>Annual Financial Reports</h1>
-    <h2>in an all-in-one, easy-to-use database</h2>
-  </section>
-  <section id="use-cases">
-    <h1>Use Cases</h1>
-    <ul>
-      <li>Scan recent reports for highly performing companies</li>
-      <li>Automate in-depth research on specific companies</li>
-      <li>Backtest investment strategies with financial data dating back to 2010</li>
-    </ul>
-  </section>
-  <section id="pricing">
-    <!--Pricing elements-->
-  </section>
+  <div id="content">
+    <section id="hero">
+      <h1>Quarterly and Annual Financial Reports</h1>
+      <h2>Gathered, processed, and stored in one easy-to-use database</h2>
+      <div style="display: flex; flex-direction: row; justify-content: space-evenly; width: 100%; margin: 1.5rem 0;">
+        <a href="#" class="button">Learn more</a>
+        <a href="#" class="button primary">See pricing</a>
+      </div>
+    </section>
+    <section id="about">
+      <h1>Features</h1>
+      <h1>Use Cases</h1>
+    </section>
+    <section id="pricing">
+    </section>
+  </div>
 </main>
 
 <style>
@@ -27,5 +28,69 @@
         --primary-text-color: white;
 
         font-family:  Roboto, sans-serif;
+    }
+
+    #hero {
+        padding: 25% 0;
+    }
+
+    #hero h1 {
+        font-size: 2.75rem;
+        margin: 0;
+    }
+
+    #hero h2 {
+        font-size: 2rem;
+        margin: 0;
+    }
+
+    a.button {
+        padding: 0.5rem;
+        border-radius: 0.25rem;
+
+        box-shadow: black 2px 2px;
+    }
+
+    .primary {
+        background-color: var(--primary-bg-color);
+        color: var(--primary-text-color);
+    }
+
+    h1 {
+        font-size: 2rem;
+        font-weight: bold;
+        font-variant-caps: small-caps;
+        text-align: center;
+        margin: 1rem;
+    }
+
+    h2 {
+        font-size: 1.5rem;
+        font-weight: normal;
+        font-variant-caps: normal;
+        text-align: center;
+    }
+
+    #content {
+        display: flex;
+        flex-direction: column;
+        justify-items: center;
+        width: 100%;
+    }
+
+    #hero {
+        display: flex;
+        flex-direction: column;
+        justify-items: center;
+    }
+
+    @media (min-width: 480px){
+        h1 {
+            font-size: 2.25rem;
+        }
+
+        #hero h1 {
+            font-size: 4rem;
+        }
     }
 </style>
