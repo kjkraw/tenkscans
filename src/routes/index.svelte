b40699fec35bd7b9cbeedc7c5f8d8eaa6b6e6276
Kuy Krawczeniuk <kjkrawczeniuk@gmail.com>
2022-03-12 22:52:54 -0800

    Outline front page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script></script>
+
+<div id="main">
+  <section id="hero">
+    <h1>Quarterly and Annual Financial Reports</h1>
+  </section>
+  <section id="about">
+    <h1>Features</h1>
+    <h1>Use Cases</h1>
+  </section>
+  <section id="pricing">
+    <h1>Pricing</h1>
+  </section>
+</div>
+
+<style>
+    #main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 2rem;
+    }
+
+    h1 {
+        font-weight: bold;
+        font-size: 2.5rem;
+        text-align: center;
+    }
+</style>
